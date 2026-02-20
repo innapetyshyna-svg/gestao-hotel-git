cab0bcb0013d7a5787eb39822ef98168cdb52ff0
innapetyshyna-svg <inna.petyshyna@gmail.com>
2026-02-20 17:42:10 +0000

    Database of rooms

---
+++ quartos.py
@@ -0,0 +1,12 @@
+## Database of rooms (Room number: Status)
+# True means Available, False means Occupied
+rooms_db = {
+    101: True,
+    102: True,
+    103: True,
+    201: True,
+    202: True,
+    203: True,
+    204: True,
+    205: True
+}
